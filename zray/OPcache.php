0a3ca23790a9411dd80caae4990c65cfc9889d56
janatzend <jan@zend.com>
2016-03-24 11:23:32 +0100

    catch if no scripts are cached,

--- zray/OPcache.php
+++ zray/OPcache.php
@@ -16,6 +16,8 @@ class OPcache
         $status = opcache_get_status();
         $config = opcache_get_configuration();
         
+        error_log(var_export($status, true), 3, '/tmp/oc.log');
+        
         $storage['opMemoryUsage'][] = $status['memory_usage'];
         $storage['opInternedStringsUsage'][] = $status['interned_strings_usage'];
         $storage['opStatistics'][] = $status['opcache_statistics'];
@@ -23,27 +25,28 @@ class OPcache
         $storage['opVersion'][] = $config['version'];
         $storage['opBlacklist'][] = $config['blacklist'];
         
-        $scripts = $status['scripts'];
+        if ($scripts = $status['scripts']) {
         
-        array_walk($scripts, function (&$item, $key)
-        {
-            $item = array_merge(array(
-                'name' => basename($item['full_path'])
-            ), array(
-                'Full Path' => $item['full_path']
-            ), $item);
-            $item['memory Consumption'] = round($item['memory_consumption'] / 1024) . " KB ({$item['memory_consumption']} B)";
-            $item['Last Used'] = $item['last_used'];
-            $item['Last Used Timestamp'] = $item['last_used_timestamp'];
-            $item['created'] = date("D M j G:i:s Y", $item['timestamp']);
-            $item['created Timestamp'] = $item['timestamp'];
-            unset($item['full_path']);
-            unset($item['memory_consumption']);
-            unset($item['last_used']);
-            unset($item['last_used_timestamp']);
-            unset($item['timestamp']);
-        });
-        
-        $storage['opcacheScripts'] = $scripts;
+            array_walk($scripts, function (&$item, $key)
+            {
+                $item = array_merge(array(
+                    'name' => basename($item['full_path'])
+                ), array(
+                    'Full Path' => $item['full_path']
+                ), $item);
+                $item['memory Consumption'] = round($item['memory_consumption'] / 1024) . " KB ({$item['memory_consumption']} B)";
+                $item['Last Used'] = $item['last_used'];
+                $item['Last Used Timestamp'] = $item['last_used_timestamp'];
+                $item['created'] = date("D M j G:i:s Y", $item['timestamp']);
+                $item['created Timestamp'] = $item['timestamp'];
+                unset($item['full_path']);
+                unset($item['memory_consumption']);
+                unset($item['last_used']);
+                unset($item['last_used_timestamp']);
+                unset($item['timestamp']);
+            });
+            
+            $storage['opcacheScripts'] = $scripts;
+        }
     }
 }
